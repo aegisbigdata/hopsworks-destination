cb441f669052b2c081d0c97524e9875e7a35911c
Manuel Polzhofer <manuel.polzhofer@fokus.fraunhofer.de>
2018-01-31 12:40:27 +0100

    update .gitignore

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mvn clean package -DskipTests
-cd ~/Downloads/streamsets-datacollector-3.0.0.0/user-libs/
-tar xvfz /home/mpo/Documents/fraunhofer/aegis/streamSetsPlayground/hopsworks-destination/target/hopsworks-destination-1.0-SNAPSHOT.tar.gz
-x hopsworks-destination/lib/hopsworks-destination-1.0-SNAPSHOT.jar
-
